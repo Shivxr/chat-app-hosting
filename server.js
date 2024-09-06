e5b9558f576be57a95451223dfa385d75c14f2ad
Shiv_Shankar <165561744+Shivxr@users.noreply.github.com>
2024-09-06 14:53:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = http.createServer(app);
+const io = socketIo(server);
 
-const io = require("socket.io")(server);
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
     console.log("A user connected");
 
     // Handle new user
-    socket.on("newuser", function (username) {
+    socket.on("newuser", (username) => {
         socket.username = username;
         io.emit("user-connected", username);
     });
 
     // Handle new message
-    socket.on("chat message", function (msg) {
+    socket.on("chat message", (msg) => {
         io.emit("chat message", { user: socket.username, message: msg });
     });
 
     // Handle user disconnection
-    socket.on("disconnect", function () {
+    socket.on("disconnect", () => {
         io.emit("user-disconnected", socket.username);
         console.log("A user disconnected");
     });
 });
 
-server.listen(5000, () => {
-    console.log("Server is running on http://localhost:5000");
+// Use the port provided by Glitch or default to 3000
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
 server.on('error', (err) => {
